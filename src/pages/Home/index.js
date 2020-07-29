2b60f5ed5c3f8b12d286c599655d725844fde620
Marcio R. F. Tiene <marciorft@gmail.com>
2020-07-28 22:34:55 -0300

    debug2

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import Menu from '../../components/Menu'
+import PageDefault from '../../components/PageDefault'
 import dadosIniciais from '../../data/dados_iniciais.json';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
-import Footer from '../../components/Footer';
+
+
 
 function Home() {
   return (
-    <div style={{ background: "#141414" }}>
-      <Menu />
+    <PageDefault>
 
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
@@ -27,22 +27,25 @@ function Home() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[3]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[4]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />      
+      />
+    </PageDefault>
 
-      <Footer />
-    </div>
+
+
+
+
   );
 }
 
