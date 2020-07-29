cbc9465ce262cbf53a9e082141b0984acc3f6a70
Marcio R. F. Tiene <marciorft@gmail.com>
2020-07-28 21:09:42 -0300

    Category registerer implementation

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
-function CadastroCategria() {
-    return (
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-        <PageDefault>
-            <h1>Pagina de cadastro de Categrias</h1>
-            <Link to='/'>
-                Ir para Home
-            </Link>
-        </PageDefault>
-    )
+function CadastroCategoria() {
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+
+      <form>
+
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
 }
 
-export default CadastroCategria
+export default CadastroCategoria;
